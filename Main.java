a8e12a3a07ac5261881f9f23d66ddd800c4dc5bb
Maxime, Jean-Pierre, Christian Jean-Pierre <58209642+pracan@users.noreply.github.com>
2022-05-23 23:36:53 +0000

    savefile fullycompleted

--- Main.java
+++ Main.java
@@ -7,119 +7,158 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println("1- New game");
-    if (entree.nextInt() == 1) {
-      Board board = new Board();
-      System.out.println("\n" + "Please, Enter the name of your game :");
-      String nameGame = entree.nextLine();
-      nameGame = entree.nextLine();
+    System.out.println("2- Recover a game");
+    int starttype = entree.nextInt();
+    System.out.println("\n" + "Please, Enter the name of your game :");
+    String nameGame = entree.nextLine();
+    nameGame = entree.nextLine();
+    String nameP1;
+    String nameP2;
+    boolean whiteturn = true;
+    boolean loop = true;
+    int counter = 0;
+    Board board = new Board();
+    SaveFile save = new SaveFile(nameGame);
+    
+    if (starttype == 1) {
       System.out.println("Please, Enter the name of your player 1 :");
-      String nameP1 = entree.nextLine();
+      nameP1 = entree.nextLine();
       System.out.println("Please, Enter the name of your player 2 :");
-      String nameP2 = entree.nextLine();
-      SaveFile save = new SaveFile(nameGame);
-
-      boolean whiteturn = true;
-      boolean loop = true;
-      System.out.println("\n" + "Begining of " + nameGame + " :\n");
-      int counter = 0;
-      // boucle de jeu.
-      while (loop) {
-        if (counter != 0) {
-          System.out.println("\n" + nameGame + " turn n° " + counter + "\n");
-        }
-        Boolean replay = true;
-        board.displayBoard();
-        int a = 0;
-        if (whiteturn) {
-          while (replay) {
-
-            System.out.println("White turn : " + nameP1 + " enter letter");
-            char c1 = entree.next().charAt(0);
-            int x1 = (int) c1;
-            if (x1 > 96) {
-              x1 -= 97;
-            } else {
-              x1 -= 65;
-            }
-            ;
-            System.out.println("White turn : " + nameP1 + " enter number");
-            int y1 = entree.nextInt();
-            y1 = 8 - y1;
-            System.out.println("White turn : Where to move now ? enter letter");
-            char c2 = entree.next().charAt(0);
-            int x2 = (int) c2;
-            if (x2 > 96) {
-              x2 -= 97;
-            } else {
-              x2 -= 65;
-            }
-            ;
-            System.out.println("White turn : enter number");
-            int y2 = entree.nextInt();
-            y2 = 8 - y2;
-            replay = board.isValidMove(y1, x1, y2, x2, whiteturn);
-
-            if (replay == false) {
-              board.validMove(y1, x1, y2, x2);
-              whiteturn = false;
-              save.WriteToFile(board.getBoard(), nameGame);
-            }
-
-            if (a == 3) {
-              System.out.println("Victory Black");
-              loop = false;
-              replay = false;
-            } else {
-              a += 1;
-            }
-          }
-
-        } else {
-          while (replay) {
-            System.out.println("Black turn : " + nameP2 + " enter letter");
-            char c1 = entree.next().charAt(0);
-            int x1 = (int) c1;
-            if (x1 > 96) {
-              x1 -= 97;
-            } else {
-              x1 -= 65;
-            }
-            ;
-            System.out.println("Black turn : " + nameP2 + " enter number");
-            int y1 = entree.nextInt();
-            y1 = 8 - y1;
-            System.out.println("Black turn : Where to move now ? enter letter");
-            char c2 = entree.next().charAt(0);
-            int x2 = (int) c2;
-            if (x2 > 96) {
-              x2 -= 97;
-            } else {
-              x2 -= 65;
-            }
-            ;
-            System.out.println("Black turn : enter number");
-            int y2 = entree.nextInt();
-            y2 = 8 - y2;
-            replay = board.isValidMove(y1, x1, y2, x2, whiteturn);
-            if (replay == false) {
-              board.validMove(y1, x1, y2, x2);
-              whiteturn = true;
-              save.WriteToFile(board.getBoard(), nameGame);
-            }
-
-            if (a == 3) {
-              loop = false;
-              replay = false;
-              System.out.println("Victory White");
-            } else {
-              a += 1;
-            }
-          }
-
-        }
-        counter++;
+      nameP2 = entree.nextLine();
+      if(save.getNewfile()){
+        save.formatWriteFile(nameP1,nameP2);
+        save.WriteToFile(board.getBoard(),0);
       }
+      System.out.println("\n" + "Begining of " + nameGame + " :\n");
+      
+    } else {
+      nameP1 = "recover it";
+      nameP2 = "recover it";
+      
+      if(save.getNewfile()==false){
+        int numberturns = save.NumberTurns();
+        if (numberturns>=12){
+          numberturns = ((numberturns - 3)/9)-1;
+        } else {
+          loop=false;
+          System.out.println("Erreur la partie ne contient aucun coup");
+        }
+        if(loop){
+          nameP1 = save.recoverPlayer1();
+          nameP2 = save.recoverPlayer2();
+          System.out.println("La partie contient "+numberturns+" coups");
+          System.out.println("Choisissez en un :");
+          int y1 = entree.nextInt();
+          //verfier entree
+          board = save.recover(y1);
+          if(y1%2==0){
+            whiteturn = true;
+          } else{
+            whiteturn = false;
+          }
+          counter = y1;
+        }
+      } else{
+        loop=false;
+        System.out.println("Erreur la partie n'as pas été trouvée");
+        }
+    };
+    // boucle de jeu.
+    while (loop) {
+      if (counter != 0) {
+        System.out.println("\n" + nameGame + " turn n° " + counter + "\n");
+      }
+      Boolean replay = true;
+      board.displayBoard();
+      int a = 0;
+      if (whiteturn) {
+        while (replay) {
 
+          System.out.println("White turn : " + nameP1 + " enter letter");
+          char c1 = entree.next().charAt(0);
+          int x1 = (int) c1;
+          if (x1 > 96) {
+            x1 -= 97;
+          } else {
+            x1 -= 65;
+          }
+          ;
+          System.out.println("White turn : " + nameP1 + " enter number");
+          int y1 = entree.nextInt();
+          y1 = 8 - y1;
+          System.out.println("White turn : Where to move now ? enter letter");
+          char c2 = entree.next().charAt(0);
+          int x2 = (int) c2;
+          if (x2 > 96) {
+            x2 -= 97;
+          } else {
+            x2 -= 65;
+          }
+          ;
+          System.out.println("White turn : enter number");
+          int y2 = entree.nextInt();
+          y2 = 8 - y2;
+          replay = board.isValidMove(y1, x1, y2, x2, whiteturn);
+
+          if (replay == false) {
+            board.validMove(y1, x1, y2, x2);
+            whiteturn = false;
+            save.WriteToFile(board.getBoard(),counter+1);
+          }
+
+          if (a == 3) {
+            System.out.println("Victory Black");
+            loop = false;
+            replay = false;
+          } else {
+            a += 1;
+          }
+        }
+
+      } else {
+        while (replay) {
+          System.out.println("Black turn : " + nameP2 + " enter letter");
+          char c1 = entree.next().charAt(0);
+          int x1 = (int) c1;
+          if (x1 > 96) {
+            x1 -= 97;
+          } else {
+            x1 -= 65;
+          }
+          ;
+          System.out.println("Black turn : " + nameP2 + " enter number");
+          int y1 = entree.nextInt();
+          y1 = 8 - y1;
+          System.out.println("Black turn : Where to move now ? enter letter");
+          char c2 = entree.next().charAt(0);
+          int x2 = (int) c2;
+          if (x2 > 96) {
+            x2 -= 97;
+          } else {
+            x2 -= 65;
+          }
+          ;
+          System.out.println("Black turn : enter number");
+          int y2 = entree.nextInt();
+          y2 = 8 - y2;
+          replay = board.isValidMove(y1, x1, y2, x2, whiteturn);
+          if (replay == false) {
+            board.validMove(y1, x1, y2, x2);
+            whiteturn = true;
+            save.WriteToFile(board.getBoard(),counter+1);
+          }
+
+          if (a == 3) {
+            loop = false;
+            replay = false;
+            System.out.println("Victory White");
+          } else {
+            a += 1;
+          }
+        }
+
+      }
+      counter++;
     }
   }
 }
--- SaveFile.java
+++ SaveFile.java
@@ -5,23 +5,31 @@ import java.io.FileNotFoundException;
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class SaveFile {
+  private String filename;
+  private boolean newfile;
+  
   public SaveFile(String name) {
     try {
       File myObj = new File(name+".txt");
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
+        this.newfile= true;
       } else {
         System.out.println("File already exists.");
+        this.newfile = false;
       }
+      this.filename = name;
     } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
+      this.newfile = false;
     }
   }
 
-  public void WriteToFile(Piece[][] board, String name) {
+  public void WriteToFile(Piece[][] board,int turn) {
     try {
-      FileWriter myWriter = new FileWriter(name+".txt");
+      FileWriter myWriter = new FileWriter(this.filename+".txt",true);
+      myWriter.write("\n"+"turn n° "+turn+"\n");
       for (int i = 0; i <= 7; i++) {
         for (int v = 0; v <= 7; v++) {
           if(v!=0){myWriter.write(" ");}
@@ -52,7 +60,7 @@ public class SaveFile {
               myWriter.write("  ");
           }
         }
-        myWriter.write("\n");
+        if (i<7){myWriter.write("\n");}
       }
       myWriter.close();
       System.out.println("Successfully wrote to the file.");
@@ -62,16 +70,49 @@ public class SaveFile {
     }
   }
 
-  public Board recover(String name) {
+  public Board recover(int turn) {
     Board board = new Board();
     try {
-      File myObj = new File(name+".txt");
+      File myObj = new File(this.filename+".txt");
       Scanner myReader = new Scanner(myObj);
+      String data = myReader.nextLine();
+      for (int i = 0; i < (turn*9)+3; i++) {
+        data = myReader.nextLine();
+      }
       for (int i = 0; i <= 7; i++) {
-        String data = myReader.nextLine();
+        data = myReader.nextLine();
         for (int v = 0; v <= 7; v++) {
-          char color = data.charAt(0+v);
-          char pawn = data.charAt(1+v);
+          char color = data.charAt(0+3*v);
+          char pawn = data.charAt(1+3*v);
+          switch (pawn){
+            case 'P' :
+              board.getBoard(i,v).setType(1);
+              break;
+            case 'R' :
+              board.getBoard(i,v).setType(2);
+              break;
+            case 'B' :
+              board.getBoard(i,v).setType(3);
+              break;
+            case 'N' :
+              board.getBoard(i,v).setType(4);
+              break;
+            case 'Q' :
+              board.getBoard(i,v).setType(5);
+              break;
+            case 'K' :
+              board.getBoard(i,v).setType(6);
+              break;
+            default :
+              board.getBoard(i,v).setType(0);
+          }
+          if (pawn != ' ') {
+            if (color == 'W'){
+              board.getBoard(i,v).setIsWhite(true);
+            } else {
+              board.getBoard(i,v).setIsWhite(false);
+            }
+          }
         }
       }
       myReader.close();
@@ -81,4 +122,74 @@ public class SaveFile {
     }
     return board;
   }
+
+  public void formatWriteFile(String player1,String player2){
+    try {
+      FileWriter myWriter = new FileWriter(this.filename+".txt");
+      myWriter.write("Begining of the game : "+filename+"\n");
+      myWriter.write("Player 1 : "+player1+"\n");
+      myWriter.write("Player 2 : "+player2);
+      myWriter.close();
+      System.out.println("Successfully wrote players info to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+
+  public boolean getNewfile(){
+    return this.newfile;
+  }
+
+  public int NumberTurns(){
+    int nbrLine = 0;
+    try {
+      File myObj = new File(this.filename+".txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        nbrLine++;
+      }
+      myReader.close();
+      return nbrLine;
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+      return -1;
+    }
+  }
+
+  public String recoverPlayer1(){
+    String name = "error";
+    try {
+      File myObj = new File(this.filename+".txt");
+      Scanner myReader = new Scanner(myObj);
+      String data = myReader.nextLine();
+      data = myReader.nextLine();
+      name = data.substring(11, data.length());
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    return name;
+  }
+
+  public String recoverPlayer2(){
+    String name = "error";
+    try {
+      File myObj = new File(this.filename+".txt");
+      Scanner myReader = new Scanner(myObj);
+      String data = myReader.nextLine();
+      data = myReader.nextLine();
+      data = myReader.nextLine();
+      name = data.substring(11, data.length());
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    return name;
+  }
+
 }
